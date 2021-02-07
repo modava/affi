3c5bcfda03e42423591b0ca0b906ab0db130d7b4
Mong Dao <mongdaovan86@yahoo.com>
2021-02-07 20:14:13 +0700

    update 07022021

--- src/components/MyErrorHandler.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace modava\affiliate\components;
-
-class MyErrorHandler extends \yii\web\ErrorHandler
-{
-    public $errorView = '@modava/affiliate/views/error/error.php';
-
-}
--- src/views/layouts/affiliate.php
+++
@@ -1,7 +0,0 @@
-<?php
-\modava\affiliate\assets\AffiliateAsset::register($this);
-\modava\affiliate\assets\AffiliateCustomAsset::register($this);
-?>
-<?php $this->beginContent('@backend/views/layouts/main.php'); ?>
-<?php echo $content ?>
-<?php $this->endContent(); ?>
